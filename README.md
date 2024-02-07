1459879ff799f0ca29d7b38cb2123eba4ded2745
Never Again <contact.nvr.again@proton.me>
2024-02-07 11:40:26 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Never Again
+
+A browser extension that highlights the products and brands related to apartheid Israel.
+
+The extension literally highlights the name of the brand, product or company on a web page. When you hover over the highlight, you are presented with an abbreviated description of the offense and a link to additional information.
+
+## Website
+
+[never-aga.in](https://never-aga.in)
---
+++ src/neverAgain.ts
@@ -0,0 +1,123 @@
+import Mark from 'mark.js';
+import { createPopper } from '@popperjs/core';
+
+interface MarkElement {
+    elem: HTMLElement,
+    popperRef: any
+}
+
+interface MarkElementsArray {
+    [index: string]: MarkElement
+}
+
+class NeverAgain {
+    private markInstance: Mark;
+    public static markElements: MarkElementsArray;
+    public static tooltipInFocus: boolean;
+    public static elemIdCount: number;
+
+    constructor() {
+        const bd: HTMLCollectionOf<HTMLElementTagNameMap['body']> = document.getElementsByTagName('body');
+        this.markInstance = new Mark(bd[0]);
+        NeverAgain.markElements = {};
+        NeverAgain.elemIdCount = 0;
+        NeverAgain.tooltipInFocus = false;
+    }
+
+    public markAll(): void {
+        this.markInstance.mark("McDonald's", {className: 'na-highlight', each: NeverAgain.eachMark, done: NeverAgain.afterMark });
+    }
+
+    public static eachMark(elem: HTMLElement): void {
+        const elemId ='na-highlight-' + NeverAgain.elemIdCount;
+        elem.id = elemId;
+        NeverAgain.markElements[elemId] = {elem: elem, popperRef: null};
+        NeverAgain.elemIdCount++;
+    };
+
+    public static afterMark(): void {
+        // Add tooltip element
+        const tooltipElem: HTMLElement = document.createElement('div');
+        tooltipElem.appendChild(document.createTextNode("McDonald's Israel feeds Zionists for free."));
+        tooltipElem.classList.add('na-tooltip');
+        document.body.appendChild(tooltipElem);
+        const link: HTMLElement = document.createElement('a');
+        link.setAttribute('href','https://www.boycotzionism.com/');
+        link.innerText = "McDonald's Israel announced that it's giving away thousands of free meals to the Israel Defence Forces and citizens.";
+        link.classList.add('a-block');
+        tooltipElem.appendChild(link);
+        // Add arrow to tooltip element
+        const tooltipArrowElem: HTMLElement = document.createElement('div');
+        tooltipArrowElem.id = 'na-arrow';
+        tooltipArrowElem.setAttribute('data-popper-arrow', '');
+        tooltipElem.appendChild(tooltipArrowElem);
+
+        NeverAgain.tooltipInFocus = false;
+
+        const showEvents: string[] = ['mouseenter', 'focus'];
+        const hideEvents: string[] = ['mouseleave', 'blur'];
+
+        showEvents.forEach(showEvent => {
+            Object.keys(NeverAgain.markElements).forEach(mElemId => {
+                NeverAgain.markElements[mElemId].elem.addEventListener(showEvent, () => {
+                    NeverAgain.create(mElemId, tooltipElem);
+                });
+            });
+
+            tooltipElem.addEventListener(showEvent, () => {
+                NeverAgain.tooltipInFocus = true;
+            });
+
+        });
+
+        hideEvents.forEach(hideEvent => {
+            Object.keys(NeverAgain.markElements).forEach(mElemId => {
+                NeverAgain.markElements[mElemId].elem.addEventListener(hideEvent, () => {
+                    NeverAgain.destroy(mElemId, tooltipElem);
+                });
+            });
+
+            tooltipElem.addEventListener(hideEvent, () => {
+                NeverAgain.destroy(<string>tooltipElem.getAttribute('data-na-referrer'), tooltipElem, true);
+            });
+        });
+    };
+
+    public static create(elemId: string, tooltipElem: HTMLElement) {
+        tooltipElem.setAttribute('data-na-show', '');
+        tooltipElem.setAttribute('data-na-referrer', elemId);
+        NeverAgain.markElements[elemId].popperRef = createPopper(NeverAgain.markElements[elemId].elem, tooltipElem, {
+            modifiers: [
+                {
+                    name: 'offset',
+                    options: {
+                        offset: [0, 8],
+                    },
+                },
+            ],
+        });
+    }
+
+    public static destroy(elemId: string, tooltipElem: HTMLElement, fromTooltip: boolean = false) {
+        if (fromTooltip) {
+            NeverAgain.destroyCore(elemId, tooltipElem);
+        } else {
+            setTimeout(() => {
+                if (!NeverAgain.tooltipInFocus && NeverAgain.markElements[elemId].popperRef) {
+                    NeverAgain.destroyCore(elemId, tooltipElem);
+                }
+            }, 500);
+        }
+    }
+
+    public static destroyCore(elemId: string, tooltipElem: HTMLElement) {
+        NeverAgain.tooltipInFocus = false;
+        tooltipElem.removeAttribute('data-na-show');
+        tooltipElem.removeAttribute('data-na-referrer');
+        NeverAgain.markElements[elemId].popperRef.destroy();
+        NeverAgain.markElements[elemId].popperRef = null;
+    }
+}
+
+const neverAgain = new NeverAgain();
+neverAgain.markAll();
